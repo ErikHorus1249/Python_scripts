964b0642931b49dc06d5b375f4d1c214a0d74859
Erik_Horus <tuananh1421999@gmail.com>
2020-12-15 16:02:47 +0700

    up

--- translate/src/main.py
+++ translate/src/main.py
@@ -2,19 +2,24 @@
 # pip3 install googletrans==3.1.0a0
 # import GG translate API
 from googletrans import Translator 
-
+import httpcore
 # thuc hien dich
 def trans(text_src):
     translator = Translator()
-    result = translator.translate(text_src, src='en', dest='vi')
-    print('='*37)
-    print(result.text)
-
+    try :
+        result = translator.translate(text_src, src='en', dest='vi')
+        return result.text
+    except httpcore._exceptions.ConnectError:
+        return '+ Checking your network cables, modem, and routers\n+ Reconnecting to your wireless network'
 # du lieu dau vao
 def getTextSRC():
     return input()
 
 if __name__ == "__main__":
-    print("\nEn -> Vi :")
-    print('='*37)
-    trans(input())
+    while(True):
+        print('='*37+'\nEn -> Vi :')
+        text_src = getTextSRC()
+        if text_src.strip()=='e' or text_src.strip()=='exit':
+            break
+        print('='*37)
+        print(trans(text_src))
