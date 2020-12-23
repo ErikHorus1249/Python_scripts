e0363f2c18154793ca0a214641b54eba6b903113
Erik_Horus <tuananh1421999@gmail.com>
2020-12-23 10:52:59 +0700

    stable

--- git_script/push.sh
+++ git_script/push.sh
@@ -3,22 +3,28 @@
 GR='\033[0;32m'
 NC='\033[0m' # No Color
 
+now=$(date +"%T")
+
 # ask massage
 echo "[+] Message ?"
 read mess
 
 if [ -z "$mess" ];
 	then
-		echo -e "${GR}[+] Successfully added content${NC}"
-        	echo -e "${GR}[+] Create a new commit containing the current contents with the default message : up ${NC}"
-		git add . && git commit -m $mess -q && git push -q
-		echo -e "${GR}[+] Successfully pushed${NC}"
+		git add .
+		echo -e "${GR}[+] $now: Successfully added content${NC}"
+		git commit -m $mess -q
+        	echo -e "${GR}[+] $now: Create a new commit containing the current contents with the default message : up ${NC}"
+		git push -q
+		echo -e "${GR}[+] $now: Successfully pushed${NC}"
 	else
-		echo -e "${GR}[+] Successfully added content${NC}"
-        	echo -e "${GR}[+] Create a new commit containing the current contents with the message : $mess ${NC}"
-		git add . && git commit -m $mess -q && git push -q
-		echo -e "${GR}[+] Successfully pushed${NC}"
+		git add .
+                echo -e "${GR}[+] $now: Successfully added content${NC}"
+                git commit -m $mess -q
+                echo -e "${GR}[+] $now: Create a new commit containing the current contents with the default message : up ${NC}"
+                git push -q
+                echo -e "${GR}[+] $now: Successfully pushed${NC}"
     fi
 
-echo -e "${GR}[+] DONE!${NC}"
+echo -e "${GR}[+] $now: DONE!${NC}"
 #Reference: https://ryanstutorials.net/bash-scripting-tutorial/bash-loops.php
