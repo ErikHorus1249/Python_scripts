3c83c84fd7aafe15f135eae8b708e51d251f602a
Erik_Horus <tuananh1421999@gmail.com>
2020-12-23 10:23:25 +0700

    none

--- git_script/push.sh
+++ git_script/push.sh
@@ -4,9 +4,9 @@ GR='\033[0;32m'
 NC='\033[0m' # No Color
 
 # ask massage
-echo Message ?
+echo "[+] Message ?"
 
 read mess
 
-echo -e "${GR}Create a new commit containing the current contents with the message : $mess ${NC}" 
+echo -e "${GR}[+] Create a new commit containing the current contents with the message : $mess ${NC}" 
 git add . && git commit -m $mess --branch && git push
