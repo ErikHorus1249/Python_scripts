6a80b98b20a8e1a2281daabdefbd1a6272586244
Erik_Horus <tuananh1421999@gmail.com>
2020-12-23 10:38:29 +0700

    stable

--- git_script/push.sh
+++ git_script/push.sh
@@ -10,10 +10,10 @@ read mess
 if [ -z "$mess" ];
 	then
         	echo -e "${GR}[+] Create a new commit containing the current contents with the default message : up ${NC}"
-		git add . && git commit -m $mess --branch && git push
+		git add . && git commit -m $mess --branch && git push -q
 	else
         	echo -e "${GR}[+] Create a new commit containing the current contents with the message : $mess ${NC}"
-		git add . && git commit -m $mess --branch && git push
+		git add . && git commit -m $mess --branch && git push -q
     fi
 
 #Reference: https://ryanstutorials.net/bash-scripting-tutorial/bash-loops.php
