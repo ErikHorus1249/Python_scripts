1a62dde7e58305bb31a634daaea34560505f1eef
erik_horus1249 <tuananh1421999@gmail.com>
2021-02-17 20:54:13 +0700

    up

--- translate/src/main.py
+++ translate/src/main.py
@@ -28,20 +28,35 @@ def get_languages():
         print('[+] '+lang+': '+languages[lang])
     src=input("CHOOSE SRC Language :").strip()
     dst=input("CHOOSE DST Language :").strip()
-    return [src,dst]
+    # kiem tra ho tro ngon ngu
+    # support_lang = True
+    src_e = ""
+    dst_e = ""
+    for lang in languages:
+        if lang == src:
+            src_e = languages[lang]
+        if lang == dst:
+            dst_e = languages[lang]
 
-def trans(text_src):
+    return [src,src_e,dst,dst_e]
+
+def trans(text_src, src, dst):
     # print(googletrans.LANGUAGES)
     translator = Translator()
     # print(languages[1])
     try :
-        result = translator.translate(text_src, src='en', dest='vi')
+        result = translator.translate(text_src, src=src, dest=dst)
         return result.text
     except httpcore._exceptions.ConnectError:
         return bcolors.WARNING + '+ Checking your network cables, modem, and routers\n+ Reconnecting to your wireless network' + bcolors.ENDC             
+
 # du lieu dau vao
 def getTextSRC():
-    return input()
+    raw_text = input()
+    if raw_text == "":
+        return "input is empty"
+    else:
+        return raw_text
 
 def showLogo():
     cwd = os.getcwd()  # Get the current working directory (cwd)
@@ -51,18 +66,20 @@ def showLogo():
     print(bcolors.FAIL+logo+bcolors.ENDC)
 
 if __name__ == "__main__":
-    # showLogo()
-    
+    # chon ngon ngu
+    LANG = get_languages()
+    Title = LANG[1] + "----->" + LANG[3]
+    # main loop
     while(True):
         # print(bcolors.OKBLUE+'='*37+'\nEn -> Vi :'+bcolors.ENDC)
         try:
             start_time = time.time()
-            print("Eng -----> Vie")
+            print(Title)
             text_src = getTextSRC()
             if text_src.strip()=='e' or text_src.strip()=='exit':
                 break
             print(bcolors.OKBLUE+'='*50+bcolors.ENDC)
-            print(bcolors.OKCYAN+trans(text_src)+bcolors.ENDC)
+            print(bcolors.OKCYAN+trans(text_src, LANG[0], LANG[2])+bcolors.ENDC)
             print("Time execution : %s s" % round(time.time() - start_time,2)+bcolors.ENDC)
         except KeyboardInterrupt:
             print("press control-c again to quit")
