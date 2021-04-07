652483f797ce4e354c7d81ddeaaf3f1bb28176fe
erik <tuananh1421999@gmail.com>
2021-04-07 17:42:31 +0700

    copy multi file

--- autoCopy/test.py
+++ autoCopy/test.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import shutil
 import time
+import glob
 
 def getUSB():
     output = subprocess.Popen("lsblk", stdout=subprocess.PIPE, shell=True)
@@ -18,12 +19,39 @@ def getFileList():
     if src != "NONE":
         files = os.listdir(src)
         for file in files:
-            if "mkv" in file:
+            if "png" in file:
                 video_list.append(file)
         return video_list
     else:
         return "NO LIST"
 
+def getObject():
+    src = getUSB()
+    parents = []
+    parents.append(src)
+    obs = os.listdir(src)
+    for ob in obs:
+        if '.' != ob:
+            getObject
+    return ob
+
+def copy(src,dst):
+    if shutil.copytree(src, dst, dirs_exist_ok=True):
+        return True
+    else:
+        return False
+
+def getRecursionFile():
+    filenames = []
+    for filename in glob.glob(str(getUSB()) + '/**/*.mkv'):
+        filenames.append(filename)
+    return filenames
+
+def getNonRecursionFile():
+    filenames = []
+    for filename in glob.glob(str(getUSB()) + '/*.mkv'):
+        filenames.append(filename)
+    return filenames
 
 status = True
 while status:
@@ -32,13 +60,11 @@ while status:
     if(src == "NONE"):
         continue
     else:
-        videos = getFileList()
+        videos = getRecursionFile() + getNonRecursionFile()
         status = False
         for video in videos:
-            shutil.copy(src+"/"+video,dest)
-            print("Copy successfully %s" % video)
+            shutil.copy(video,dest)
             time.sleep(0.5)
-
 
 
 
